491a3a3bd21b06ae3a7108ce5ea7a34abaf57bea
Ardzs Obedencia <ardeeroiobedencia901@gmail.com>
2026-04-14 23:12:42 +0800

    new commit

---
+++ Multiple_motor_code/Multiple_motor_code.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <iostream>
+ 
+int TOF1 = 2;
+int TOF2 = 3;
+int TOF3 = 4;
+int SENS1 = 5;
+int SENS2 = 6;
+int SENS3 = 7;
+int SENS4 = 8;
+
+void setup() {
+  pinMode(TOF1, OUTPUT);
+  pinMode(TOF2, OUTPUT);
+  pinMode(TOF3, OUTPUT);
+  pinMode(SENS1, INPUT);
+  pinMode(SENS2, INPUT);
+  pinMode(SENS3, INPUT);
+  pinMode(SENS4, INPUT);  
+}
+void loop() {}  //ihfiehfui
---
+++ Single_motor_code/Single_motor_code.ino
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <iostream>
+
+int nigpin = 5;
+
+void setup(){
+  pinMode(nigpin,OUTPUT);
+}
+void loop(){
+  digitalWrite(nigpin,HIGH);
+  delay(1000);
+}
